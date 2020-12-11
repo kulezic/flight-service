ea8cfd94b3b2ef417a3cd342cf6e5930ca789ea9
kuleic <andrijana.kulezic@gmail.com>
2020-12-11 17:58:21 +0100

    Initial commit

---
+++ src/test/java/com/a2/flightservice/FlightServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.a2.flightservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
